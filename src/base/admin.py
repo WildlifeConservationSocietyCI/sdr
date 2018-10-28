612542dba2a22c82de2c578b8f4b53f93109101e
Kim Fisher <kfisher@wcs.org>
2018-10-28 12:06:28 -0400

    Separated reference inline into delete/add lists to speed up query

--- src/base/admin.py
+++ src/base/admin.py
@@ -57,33 +57,81 @@ class SpeciesReferenceInlineFormset(BaseInlineFormSet):
         super(SpeciesReferenceInlineFormset, self).__init__(*args, **kwargs)
         self.queryset = self.queryset.prefetch_related(
             Prefetch('species', queryset=Species.objects.all().only('name_accepted', 'name_common')),
-            Prefetch('reference', queryset=Reference.objects.all().only('name'))
-        )
+            Prefetch('reference', queryset=Reference.objects.all().only('id')),
+            Prefetch('period', queryset=Period.objects.all()),
+        ).order_by('species__name_accepted')
 
 
+# class SpeciesReferenceInline(SdrTabularInline):
+#     model = SpeciesReference
+#     fields = ('species', 'reference', 'distribution', 'period', 'pagenumbers', 'notes')
+#     extra = 0
+#     formset = SpeciesReferenceInlineFormset
+#
+#     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+#         field = super(SpeciesReferenceInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+#         if db_field.name == 'species' and hasattr(self, 'cached_species'):
+#             field.choices = self.cached_species
+#         return field
+
+
+# noinspection PyProtectedMember
 class SpeciesReferenceInline(SdrTabularInline):
     model = SpeciesReference
+    formset = SpeciesReferenceInlineFormset
+    readonly_fields = ('species_link', 'reference', 'distribution', 'period', 'pagenumbers', 'notes')
+    exclude = ('created_by', 'species')
+
+    def species_link(self, obj):
+        link = reverse('admin:%s_%s_change' % ('species', 'species'), args=(obj.species.pk,))
+        link_formatted = format_html('<a href="{0}">{1}</a>', link, obj.species)
+        return mark_safe(link_formatted)
+    species_link.admin_order_field = 'species__name_accepted'
+    species_link.short_description = 'species'
+
+    def has_add_permission(self, request):
+        return False
+
+
+class AddSpeciesReferenceInline(SdrTabularInline):
+    model = SpeciesReference
+    formset = SpeciesReferenceInlineFormset
     fields = ('species', 'reference', 'distribution', 'period', 'pagenumbers', 'notes')
     extra = 0
-    formset = SpeciesReferenceInlineFormset
+    verbose_name_plural = 'Add species references'
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        field = super(SpeciesReferenceInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
-        if db_field.name == 'species' and hasattr(self, 'cached_species'):
-            field.choices = self.cached_species
-        return field
+    def get_queryset(self, request):
+        return super(AddSpeciesReferenceInline, self).get_queryset(request).none()
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 class ReferenceAdmin(ReferenceAdmin, SpeciesReferenceMixin):
     list_display = ('name_short', 'zotero_link', 'last_modified')
     list_display_links = ('name_short',)
     search_fields = ['name_short', 'zotero']
-    inlines = [SpeciesReferenceInline, ]
 
-    def get_formsets_with_inlines(self, request, obj=None):
-        for inline in self.get_inline_instances(request, obj):
-            inline.cached_species = [(i.pk, str(i)) for i in Species.objects.only('name_accepted', 'name_common')]
-            yield inline.get_formset(request, obj), inline
+    # def get_formsets_with_inlines(self, request, obj=None):
+    #     for inline in self.get_inline_instances(request, obj):
+    #         inline.cached_species = [(i.pk, str(i)) for i in Species.objects.only('name_accepted', 'name_common')]
+    #         yield inline.get_formset(request, obj), inline
+
+    def add_view(self, request, form_url='', extra_context=None):
+        self.inlines = []
+        if '_popup' not in request.GET:
+            self.inlines = [SpeciesReferenceInline, AddSpeciesReferenceInline]
+        return super(ReferenceAdmin, self).add_view(
+            request, form_url, extra_context=extra_context,
+        )
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        self.inlines = []
+        if '_popup' not in request.GET:
+            self.inlines = [SpeciesReferenceInline, AddSpeciesReferenceInline]
+        return super(ReferenceAdmin, self).change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        )
 
 
 admin.site.unregister(User)
